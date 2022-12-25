e62c80277891afa5d9581456c4cc7f349378e306
DenaBaniBakr <116040959+DenaBaniBakr@users.noreply.github.com>
2022-12-25 17:26:59 +0300

    comment

--- Program.cs
+++ Program.cs
@@ -8,9 +8,59 @@ namespace Online_Shoping_Site
         //second comment test
         //third comment test
         //comment tset
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+/*public void LoggedAsCustomer()
+{
+int choice = 0;
+do
+{
+Console.WriteLine("1. View all available listings.");
+Console.WriteLine("2. View a chosen listing information.");
+Console.WriteLine("3. Add a listing to their cart.");
+Console.WriteLine("4. View/Edit added listings to their cart.");
+Console.WriteLine("5. Checkout listings.");
+Console.WriteLine("6. Change account information.");
+Console.WriteLine("7. search for a listing.");
+Console.WriteLine("5. logout.");
+Console.Write("\nEnter your choice: ");
+choice = Convert.ToInt16(Console.ReadLine());
+switch (choice)
+{
+case 1:
+ViewAllAvailableListings();
+break;
+case 2:
+ViewChosenListingInformation();
+break;
+case 3:
+AddListingToCart();
+break;
+case 4:
+ViewEditAddedListingsToCart();
+break;
+case 5:
+CheckoutListings();
+break;
+case 6:
+ChangeAccountInformation();
+break;
+case 7:
+SearchForListing();
+break;
+case 8:
+break;
+default:
+Console.WriteLine("Invalid Choice, please try agian...");
+break;
+}
+}
+while (choice != 8);
+}
+*/
+
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello World!");
+}
+}
 }
