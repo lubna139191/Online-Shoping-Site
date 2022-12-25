467a22506cd262491c18784dc0f4061b1a0bf777
DenaBaniBakr <116040959+DenaBaniBakr@users.noreply.github.com>
2022-12-26 01:31:47 +0300

    general structure

--- Program.cs
+++ Program.cs
@@ -7,6 +7,154 @@ using System.Runtime.Serialization;
 
 namespace Online_Shoping_Site
 {
+    class Seller
+    {
+        int PhoneNumber;
+        string Name;
+        string address;
+        string EmailAddress;
+        string StoreName;
+        string listings;
+
+        public void SignUpSeller()
+        {
+
+        }
+        public void LogInInfoSeller()
+        {
+
+        }
+
+        public void loggedSeller()
+        {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. Adding new listing.");
+                Console.WriteLine("2. Delete existing listing");
+                Console.WriteLine("3. Change info and price for existing listing.");
+                Console.WriteLine("4. View all listings.");
+                Console.WriteLine("5. View sold listings information.");
+                Console.WriteLine("6. Change account information.");
+                Console.WriteLine("7. logout.");
+                Console.Write("\nEnter your choice: ");
+                choice = Convert.ToInt16(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //AddingNewListing();
+                        break;
+                    case 2:
+                        //DeleteExistingListing();
+                        break;
+                    case 3:
+                        //ChangeInfoAndPriceForExistingListing();
+                        break;
+                    case 4:
+                        //ViewAllListings();
+                        break;
+                    case 5:
+                        //ViewSoldListingsInformation();                   
+                        break;
+                    case 6:
+                        //ChangeAccountInformation();                   
+                        break;
+                    case 7:
+                        //ViewSoldListingsInformation();                   
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice, please try agian...");
+                        break;
+                }
+            }
+            while (choice != 7);
+        }
+       
+    }
+//***************************************************
+    class customer
+    {
+        int CustomerId;
+        int PhoneNumber;
+        string Name;
+        string ShippingAddress;
+        string EmailAddress;
+        string Password;
+        string PaymentInformation;
+
+        public void SignUpCustomer()
+        {
+
+        }
+
+        public void LogInInfoCustomer()
+        {
+
+        }
+
+        public void LoggedAsCustomer()
+        {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. View all available listings.");
+                Console.WriteLine("2. View a chosen listing information.");
+                Console.WriteLine("3. Add a listing to their cart.");
+                Console.WriteLine("4. View/Edit added listings to their cart.");
+                Console.WriteLine("5. Checkout listings.");
+                Console.WriteLine("6. Change account information.");
+                Console.WriteLine("7. search for a listing.");
+                Console.WriteLine("8. logout.");
+                Console.Write("\nEnter your choice: ");
+                choice = Convert.ToInt16(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //ViewAllAvailableListings();
+                        break;
+                    case 2:
+                        //ViewChosenListingInformation();
+                        break;
+                    case 3:
+                        //AddListingToCart();
+                        break;
+                    case 4:
+                        //ViewEditAddedListingsToCart();
+                        break;
+                    case 5:
+                        //CheckoutListings();
+                        break;
+                    case 6:
+                        //ChangeAccountInformation();
+                        break;
+                    case 7:
+                        //SearchForListing();
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice, please try agian...");
+                        break;
+                }
+            }
+            while (choice != 8);
+        }
+    }
+
+//***************************************************
+        class Payment
+    {
+  
+    }
+//***************************************************
+        class Listings
+    {
+       
+    }
+ //***************************************************
+
     class Program
     {
         //First comment test 
@@ -28,54 +176,55 @@ namespace Online_Shoping_Site
             Console.WriteLine("Enter your choice: ");
             int user = Convert.ToInt32(Console.ReadLine());
 
-            switch (user)
+            if (user == 1 || user == 2)
             {
-                case 1:
-                    Console.WriteLine("If this is your first visit to the site,");
-                    Console.WriteLine("choose the first option (Sign Up),");
-                    Console.WriteLine("If you have already registered, choose the second option (Log In).");
-                    Console.WriteLine("1. SignUp.");
-                    Console.WriteLine("2. Login.");
-                    int Action = Convert.ToInt32(Console.ReadLine());
-                    if (Action == 1)
-                    {
-
-                    }
-                    else if (Action == 2)
-                    {
-
-                    }
-                    else
-                        Console.WriteLine("Wrong choice");
-                    break;
-                case 2:
-                    Console.WriteLine("If this is your first visit to the site,");
-                    Console.WriteLine("choose the first option (Sign Up),");
-                    Console.WriteLine("If you have already registered, choose the second option (Log In).");
-                    Console.WriteLine("1. SignUp.");
-                    Console.WriteLine("2. Login.");
-                    int Choice = Convert.ToInt32(Console.ReadLine());
-                    if (Choice == 1)
-                    {
-
-                    }
-                    else if (Choice == 2)
-                    {
-
-                    }
-                    else
-                        Console.WriteLine("Wrong choice");
-                    break;
+                switch (user)
+                {
+                    case 1:
+                        Console.WriteLine("If this is your first visit to the site,");
+                        Console.WriteLine("choose the first option (Sign Up),");
+                        Console.WriteLine("If you have already registered, choose the second option (Log In).");
+                        Console.WriteLine("1. SignUp.");
+                        Console.WriteLine("2. Login.");
+                        int Action = Convert.ToInt32(Console.ReadLine());
+                        if (Action == 1)
+                        {
+                            //SignUpSeller()
+                        }
+                        else if (Action == 2)
+                        {
+                            //LogInSeller()
+                        }
+                        else
+                            Console.WriteLine("Invalid Choice, please try agian...");
+                        break;
+                    case 2:
+                        Console.WriteLine("If this is your first visit to the site,");
+                        Console.WriteLine("choose the first option (Sign Up),");
+                        Console.WriteLine("If you have already registered, choose the second option (Log In).");
+                        Console.WriteLine("1. SignUp.");
+                        Console.WriteLine("2. Login.");
+                        int Choice = Convert.ToInt32(Console.ReadLine());
+                        if (Choice == 1)
+                        {
+                            //SignUpCustomer()
+                        }
+                        else if (Choice == 2)
+                        {
+                            //LogInCustomer()
+                        }
+                        else
+                            Console.WriteLine("Invalid Choice, please try agian...");
+                        break;
+                }
             }
+            else
+                Console.WriteLine("Invalid Choice, please try agian...");
         }
 
     }
 }
-            /*string
-            in the main, create boolean variable(name:tryagain) with initial value True
-            then in while loop with the boolean variable as condition
-            ask if the user is seller or customer,
-            then ask if the user want to signup or login.*/
+
 
 
 
